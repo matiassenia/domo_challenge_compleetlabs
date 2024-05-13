8b6d00a05b9bbb6c72be719650d4f3f34c6305f6
Matías Senia <matiassenia9@gmail.com>
2024-05-13 10:33:59 -0300

    responsive array

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ function triggerAnimation(entries){
 const options= {
     root: null,
     rootMargin: "0px",
-    threshold: 1, 
+    threshold: 0.5, 
 }
 const observer = new IntersectionObserver(triggerAnimation, options);
 
